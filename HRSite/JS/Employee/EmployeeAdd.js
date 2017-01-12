85bc5f1c1bb2f22e9d6b6d25ade8fe4fdfbf7040
Admin <Admin@PC-YANGXF>
2017-01-12 18:22:48 +0800

    20170112

--- HRSite/JS/Employee/EmployeeAdd.js
+++ HRSite/JS/Employee/EmployeeAdd.js
@@ -1,3 +1,51 @@
 ﻿$(document).ready(function () {
-    $("#tmEntryDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd",width:120 });
+    $("#txbEmpName").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbCardNo").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbAddress").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbPhone").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbPhone").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbJobs").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbHouseAccount").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbBankAccount").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbContractNo").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbInsCardNo").jqxInput({ minLength: 1, height: 25, width: 200 });
+    $("#txbEmpEmail").jqxInput({ minLength: 1, height: 25, width: 200 });
+
+    $("#tmEntryDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+
+    $("#tmConStartDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmConEndDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmLeaveDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmSocialStartDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmHouseStartDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmEmployDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+    $("#tmSocialSignDate").jqxDateTimeInput({ formatString: "yyyy-MM-dd" });
+
+    $("#numTotalAmount").jqxNumberInput({ decimalDigits: 2, Digits: 6, spinButtons: true });
+    $("#numSocialFundNum").jqxNumberInput({ decimalDigits: 2, Digits: 6, spinButtons: true });
+    $("#numHouseFundNum").jqxNumberInput({ decimalDigits: 2, Digits: 6, spinButtons: true });
+
+    var currencyUrl = "../BaseData/sex.txt";
+    var currencySource = { datatype: "json", datafields: [{ name: "SexId" }, { name: "SexName" }], url: currencyUrl, async: false };
+    var currencyDataAdapter = new $.jqx.dataAdapter(currencySource);
+    $("#selSex").jqxComboBox({ source: currencyDataAdapter, displayMember: "SexName", valueMember: "SexId", autoComplete: true, searchMode: "contains", selectedIndex: 0 });
+
+    var currencyUrl = "../BaseData/degree.txt";
+    var currencySource = { datatype: "json", datafields: [{ name: "DegreeId" }, { name: "DegreeName" }], url: currencyUrl, async: false };
+    var currencyDataAdapter = new $.jqx.dataAdapter(currencySource);
+    $("#SelDegree").jqxComboBox({ source: currencyDataAdapter, displayMember: "DegreeName", valueMember: "DegreeId", autoComplete: true, searchMode: "contains", selectedIndex: 0 });
+
+    var currencyUrl = "../BaseData/bank.txt";
+    var currencySource = { datatype: "json", datafields: [{ name: "BankId" }, { name: "BankName" }], url: currencyUrl, async: false };
+    var currencyDataAdapter = new $.jqx.dataAdapter(currencySource);
+    $("#selBank").jqxComboBox({ source: currencyDataAdapter, displayMember: "BankName", valueMember: "BankId", autoComplete: true, searchMode: "contains"});
+
+    var currencyUrl = "../BaseData/payCity.txt";
+    var currencySource = { datatype: "json", datafields: [{ name: "payCityId" }, { name: "payCityName" }], url: currencyUrl, async: false };
+    var currencyDataAdapter = new $.jqx.dataAdapter(currencySource);
+    $("#selPayCity").jqxComboBox({ source: currencyDataAdapter, displayMember: "payCityName", valueMember: "payCityId", autoComplete: true, searchMode: "contains", selectedIndex: 0 });
+
+    $("#chkIsHandBook").jqxCheckBox();
+    $("#chkIsResidentPermit").jqxCheckBox();
+    $("#chkIsBirthIns").jqxCheckBox();
 });
---
+++ SQLServerDAL/EmployeeSQL.cs
@@ -0,0 +1,42 @@
+﻿using HR.DBUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.Model;
+using System.Data.SqlClient;
+
+namespace HR.SQLServerDAL
+{
+    public class EmployeeSQL
+    {
+        private string ConnectString
+        {
+            get
+            {
+                return SQLConnString.GetConnSting();
+            }
+        }
+        private Employee SetOneRow(SqlDataReader rd)
+        {
+            Employee employee = new Employee();
+            if (rd["EmpId"] != DBNull.Value)
+            {
+                employee.EmpId = int.Parse(rd["EmpId"].ToString());
+            }
+            employee.EmpName = rd["EmpName"].ToString();
+            if (rd["Sex"] != DBNull.Value)
+            {
+                employee.Sex = int.Parse(rd["Sex"].ToString());
+            }
+            employee.CardNo = rd["CardNo"].ToString();
+            employee.Address = rd["Address"].ToString();
+            if (rd["EmpStatus"] != DBNull.Value)
+            {
+                employee.EmpStatus = int.Parse(rd["EmpStatus"].ToString());
+            }
+            return employee;
+        }
+    }
+}
